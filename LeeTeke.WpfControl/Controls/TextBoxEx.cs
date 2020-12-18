8b8ae974c9f96a0944851795808c94eef350a13a
leeteke <lifuyudemenghuan@163.com>
2020-12-18 17:43:28 +0800

    修改TBing

---
+++ LeeTeke.WpfControl/Controls/TextBoxEx.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LeeTeke.WpfControl.Controls
+{
+    /// <summary>
+    /// 按照步骤 1a 或 1b 操作，然后执行步骤 2 以在 XAML 文件中使用此自定义控件。
+    ///
+    /// 步骤 1a) 在当前项目中存在的 XAML 文件中使用该自定义控件。
+    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根
+    /// 元素中:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:LeeTeke.WpfControl.Controls"
+    ///
+    ///
+    /// 步骤 1b) 在其他项目中存在的 XAML 文件中使用该自定义控件。
+    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根
+    /// 元素中:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:LeeTeke.WpfControl.Controls;assembly=LeeTeke.WpfControl.Controls"
+    ///
+    /// 您还需要添加一个从 XAML 文件所在的项目到此项目的项目引用，
+    /// 并重新生成以避免编译错误:
+    ///
+    ///     在解决方案资源管理器中右击目标项目，然后依次单击
+    ///     “添加引用”->“项目”->[浏览查找并选择此项目]
+    ///
+    ///
+    /// 步骤 2)
+    /// 继续操作并在 XAML 文件中使用控件。
+    ///
+    ///     <MyNamespace:TextBoxEx/>
+    ///
+    /// </summary>
+    public class TextBoxEx : TextBox
+    {
+        static TextBoxEx()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TextBoxEx), new FrameworkPropertyMetadata(typeof(TextBoxEx)));
+        }
+
+
+       
+
+
+        public TextBoxEx()
+        {
+
+        }
+
+
+        #region 依赖属性
+
+
+        #region Mode
+        /// <summary>
+        /// 请填写描述
+        /// </summary>
+        public TextMode Mode
+        {
+            get { return (TextMode)GetValue(ModeProperty); }
+            set { SetValue(ModeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Mode.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ModeProperty =
+            DependencyProperty.Register("Mode", typeof(TextMode), typeof(TextBoxEx), new PropertyMetadata(TextMode.General));
+
+        #endregion
+
+
+        #region Text
+
+        #region Text
+        /// <summary>
+        /// 请填写描述
+        /// </summary>
+        public new string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public new static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(TextBoxEx));
+
+        #endregion
+
+        #endregion
+
+
+        #region PasswordChar
+        /// <summary>
+        /// 请填写描述
+        /// </summary>
+        public char PasswordChar
+        {
+            get { return (char)GetValue(PasswordCharProperty); }
+            set { SetValue(PasswordCharProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for PasswordChar.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PasswordCharProperty =
+            DependencyProperty.Register("PasswordChar", typeof(char), typeof(TextBoxEx));
+
+        #endregion
+
+
+        #region PreviewText
+        /// <summary>
+        /// 请填写描述
+        /// </summary>
+        public string PreviewText
+        {
+            get { return (string)GetValue(PreviewTextProperty); }
+            set { SetValue(PreviewTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for PreviewText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PreviewTextProperty =
+            DependencyProperty.Register("PreviewText", typeof(string), typeof(TextBoxEx));
+        #endregion
+
+
+
+        #endregion
+
+    }
+}
---
+++ LeeTeke.WpfControl/Models/TextMode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeeTeke.WpfControl
+{
+    public enum TextMode
+    {
+        /// <summary>
+        /// 常规
+        /// </summary>
+        General,
+        /// <summary>
+        /// 密码行为
+        /// </summary>
+        Password,
+        /// <summary>
+        /// 数字行为
+        /// </summary>
+        Number,
+        /// <summary>
+        /// 禁止IME
+        /// </summary>
+        IMEDispaly,
+    }
+}
