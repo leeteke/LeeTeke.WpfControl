9e49542827fa33b8774b3b7a7da1009e2ea53ecf
leeteke <lifuyudemenghuan@163.com>
2022-05-16 15:04:03 +0800

    修复ListItemMargin错误

--- LeeTeke.WPFTest/TestWindow.xaml.cs
+++ LeeTeke.WPFTest/TestWindow.xaml.cs
@@ -20,57 +20,10 @@ namespace LeeTeke.WPFTest
     /// </summary>
     public partial class TestWindow : Window
     {
-        private ObservableCollection<bool>  items= new ObservableCollection<bool>() { false,true,true,true, true,true };
+        private ObservableCollection<bool> items = new ObservableCollection<bool>() { false, true, true, true, true, true };
         public TestWindow()
         {
             InitializeComponent();
-            this.Loaded += TestWindow_Loaded;
-        }
-
-        private void TestWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            test.ItemsSource = items;
-
-            for (int i = 0; i < 100; i++)
-            {
-                tb.AppendText($"{i}\r\n");
-            }
-      
-        }
-
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void test_SelectionChanged(object sender, WpfControl.ToggleSelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            LeeTeke.WpfControl.MessageBoxEx.Show("测试");
-        }
-
-        private void Navigation_ItemSelected(object sender, WpfControl.NavigationItemSelectedEventArgs e)
-        {
-            tb.AppendText($"S__{DateTime.Now:HH:mm:ss:fff}__{e.Value}\r\n");
-        }
-
-        private void test_ItemClosed(object sender, WpfControl.NavigationItemColsedEventArgs e)
-        {
-            tb.AppendText($"C__{DateTime.Now:HH:mm:ss:fff}__{e.Value}\r\n");
-        }
-
-        private void Navigation_DragDropOver(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void test_SelectionChanged_1(object sender, WpfControl.ToggleSelectionChangedEventArgs e)
-        {
-
         }
     }
 }
