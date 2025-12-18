e54c21213b13f192653819d27dd9f93df5c0b240
leeteke <leeteke@163.com>
2025-12-18 16:00:37 +0800

    增加一个IsEnabled隔离控价，优化主题IsEnabled=false的样式

---
+++ LeeTeke.WpfControl/Controls/KeepIsEnabled.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LeeTeke.WpfControl.Controls
+{
+    public class KeepIsEnabled : ContentControl
+    {
+
+        static KeepIsEnabled()
+        {
+            IsEnabledProperty.OverrideMetadata(typeof(KeepIsEnabled), new UIPropertyMetadata(true, (_, __) => { }, (_, x) => x));
+        }
+    }
+}
